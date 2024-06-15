c90d9d51dd0335f6b7716c4aab47899b8ab8a186
Kovida Tyagi <141024193+kovida09@users.noreply.github.com>
2024-06-15 07:14:24 +0000

    hub

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../desktop-wallpaper-portfolio.jpg";
+import image from "../images/desktop-wallpaper-portfolio.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
